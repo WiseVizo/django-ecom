a16109d272efb65fb94bb0347f3ad07150600390
WiseVizo <mecoc1011@gmail.com>
2024-04-23 01:26:21 +0530

    implemented search

--- ecomsite/product/urls.py
+++ ecomsite/product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
 app_name = "product"
 urlpatterns = [
     path("", ProductListView.as_view(), name="products"),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- ecomsite/product/views.py
+++ ecomsite/product/views.py
@@ -1,9 +1,33 @@
-from django.views.generic import ListView
+from django.db.models.query import QuerySet
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Product
-from django.core.paginator import Paginator
+
 
 class ProductListView(ListView):
     model = Product
     template_name = 'product/index.html'
     context_object_name = 'products'
     paginate_by = 3  # Number of products per page
+    
+    # def get_queryset(self):
+    #     query = self.request.POST.get('search')
+    #     print(f'query: {query}')
+    #     if query:
+    #         return Product.objects.filter(name__icontains=query)
+    #     else:
+    #         return Product.objects.all()
+        
+    def post(self, request, *args, **kwargs):
+        # Handle POST request for search form submission
+        query = self.request.POST.get('search')
+        if query:
+            products = Product.objects.filter(name__icontains=query)
+        else:
+            products = Product.objects.all()
+        return render(request, self.template_name, {'products': products})
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product/detail.html' 
+    context_object_name = 'product' 
