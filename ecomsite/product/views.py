3205659ac3da912b31fd843a3340fa22c905c446
WiseVizo <mecoc1011@gmail.com>
2024-04-22 23:45:12 +0530

    product listing page done

--- ecomsite/product/views.py
+++ ecomsite/product/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import ListView
 from .models import Product
+from django.core.paginator import Paginator
 
 class ProductListView(ListView):
     model = Product
     template_name = 'product/index.html'
     context_object_name = 'products'
+    paginate_by = 3  # Number of products per page
